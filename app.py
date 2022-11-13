93af79c490cf3fefe31a4b56632c1b4ee033850c
lechithinh <lechithinh.developer@gmail.com>
2022-11-13 19:28:40 +0700

    Init colab environment

---
+++ app.py
@@ -0,0 +1,138 @@
+from fastapi import FastAPI
+from api_module import *
+from helpers import *
+from fastapi import FastAPI, UploadFile, File, Response
+from PIL import Image
+import numpy as np
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
+import io
+import pandas as pd
+import subprocess
+from fastapi import FastAPI, Request
+from starlette.responses import RedirectResponse
+from pydantic import BaseModel
+
+class Payload(BaseModel):
+    csv: str
+
+app = FastAPI()
+model = Searching()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/text")
+async def read_item(
+    text_query: str,
+    topk: int = 20,
+    object_filter: str = "",
+    voice_filter: str = "",
+    color_filter: str = "",
+    swap_top: str = "",
+):
+    images, paths, csv = model.search(
+        text_query=text_query,
+        top_k=int(topk),
+        objects_filter_text=object_filter,
+        voice_filter_text=voice_filter,
+        color_filter=color_filter,
+        swap_top=swap_top,
+    )
+
+    return {"paths": paths, "csv": csv}
+
+
+@app.post("/image")
+async def read_item(
+    file: UploadFile = File(...),
+    topk: int = 20,
+    object_filter: str = "",
+    voice_filter: str = "",
+    color_filter: str = "",
+    swap_top: str = "",
+):
+    image_query = convert_image(await file.read())
+    images, paths, csv = model.search(
+        image_query=image_query,
+        top_k=int(topk),
+        objects_filter_text=object_filter,
+        voice_filter_text=voice_filter,
+        color_filter=color_filter,
+        swap_top=swap_top,
+    )
+
+    return {"paths": paths, "csv": csv}
+
+@app.post("/drawing") #route này buộc phải có filter mới chạy nổi ví dụ : Chopsticks (object)
+async def read_item(
+    file: UploadFile = File(...),
+    topk: int = 20,
+    object_filter: str = "",
+    voice_filter: str = "",
+    color_filter: str = "",
+    swap_top: str = "",
+):
+    image_query = np.array(Image.open(BytesIO(await file.read())))
+    images, paths, csv = model.search(
+        drawing_query=image_query,
+        top_k=int(topk),
+        objects_filter_text=object_filter,
+        voice_filter_text=voice_filter,
+        color_filter=color_filter,
+        swap_top=swap_top,
+    )
+
+    return {"paths": paths, "csv": csv}
+
+@app.get("/stream/{_:path}")
+async def show_image(path: str):
+    try:
+        image = Image.open(r"{}".format(path))
+        buf = BytesIO()
+        image.save(buf, format="JPEG")
+
+        return Response(content=buf.getvalue(), media_type="image/jpg")
+    except Exception as e:
+        print(e)
+        return "Error when stream image: {}".format(path)
+
+
+@app.get("/explore/{_:path}")
+def open_folder(path: str = ""):
+    path = path[:-12] #chỗ này không cố định, cần kiểm tra xóa cái đuôi ảnh ra
+    print("path", path)
+    try:
+        os.startfile(path)
+        return {"status": "sucess"}
+    except Exception as e:
+        print(e)
+        subprocess.Popen(["xdg-open", path])
+        return {"status": "sucess1"}
+
+@app.get("/ytb/{_:path}")
+def redirect_link(path: str):
+    # path: <tên video>/<Keyframe ID>.jpg Ví dụ: C00_V0135/029928.jpg
+    video_name = path.split("/")[0]
+    link_path = f'{META_LINK}\{video_name}.json'
+    f = open(link_path, encoding="utf8")
+    data = json.load(f)
+    url = data['watch_url']
+    print(url)
+    return RedirectResponse(url)
+
+
+
+@app.post("/map")
+def csv_mapping(payload: Payload):
+    TESTDATA = payload.csv
+    df = reverse_to_csv(TESTDATA)
+    map_df = map_keyframe(df)
+    csv = map_df.to_csv(header=False, index=False).encode('utf-8')
+    return {"csv": csv}
---
+++ helpers.py
@@ -0,0 +1,121 @@
+
+import cv2
+import numpy as np
+from io import BytesIO
+from typing import List, Tuple
+import matplotlib.pyplot as plt
+import PIL.Image
+import pandas as pd
+import webcolors
+import os
+
+import pandas as pd
+import os
+from tqdm import tqdm
+
+def standardize_frameid(frameid):
+    return str("000000" + str(frameid))[-6:]
+
+
+def bring_to_top(a, i):
+    return [a[i-1]] + a[:i-1] + a[i:]
+
+def bring_list_to_top(a, b):
+    for i in range(len(b)-1, -1, -1):
+        vt = b[i] + sum([1 if b[j] > b[i] else 0 for j in range(len(b)-1, i, -1)])
+        a = bring_to_top(a, vt)
+        
+    return a
+
+
+def uncompress(x):
+    res = x.split('_')
+    return (res[0] + '_' + res[1], standardize_frameid(res[2]) + '.jpg')
+
+def gallery(array, ncols=5):
+    assert array.shape[0] != 0
+    
+    extra_array = []
+    base_h, base_w = array[0].shape[:2]
+    
+    for i in range(array.shape[0]):
+        extra_array.append(cv2.resize(array[i], dsize=(base_w, base_h)))
+
+    while len(extra_array) % ncols != 0:
+        extra_array.append(np.zeros((base_h, base_w, 3), dtype = "uint8"))
+    
+    array = np.array(extra_array)
+    nindex, height, width, intensity = array.shape
+
+    nrows = nindex//ncols
+    assert nindex == nrows*ncols
+    # want result.shape = (height*nrows, width*ncols, intensity)
+    result = (array.reshape(nrows, ncols, height, width, intensity)
+              .swapaxes(1,2)
+              .reshape(height*nrows, width*ncols, intensity))
+    return result
+
+def visualize_qt(imgs: List[PIL.Image.Image, ]) -> None:
+    #plt.figure(figsize=(20, 9))
+    plt.figure(figsize=(10, 5))
+    plt.axis('off')
+    plt.tight_layout(h_pad=0.2, w_pad=0.2)
+    result = gallery(np.array(imgs))
+    plt.imshow(result)
+    plt.show()
+
+
+def convert_image(data):
+    return PIL.Image.open(BytesIO(data))
+
+def get_csv(search_result):
+    submits = []
+    for res in search_result:
+      videoName = f"{res['video_name']}.mp4"
+      image_file = res["keyframe_id"]
+      frameName = image_file.split('.')[0]
+      submits.append([videoName, frameName])
+
+    df = pd.DataFrame(submits)
+    #return df
+    return df.to_csv(header=False, index=False).encode('utf-8')
+
+
+#Phần này bỏ qua 
+def closest_colour(requested_colour):
+    min_colours = {}
+    for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - requested_colour[0]) ** 2
+        gd = (g_c - requested_colour[1]) ** 2
+        bd = (b_c - requested_colour[2]) ** 2
+        min_colours[(rd + gd + bd)] = name
+    return min_colours[min(min_colours.keys())]
+
+def get_colour_name(requested_colour):
+    try:
+        closest_name = actual_name = webcolors.rgb_to_name(requested_colour)
+    except ValueError:
+        closest_name = closest_colour(requested_colour)
+        actual_name = None
+    return actual_name, closest_name
+
+#======================
+
+
+#map keyframe utils
+def reverse_to_csv(TESTDATA):
+    submits = []
+    for sample in TESTDATA.split("\r\n"):
+        submits.append(sample.split(','))
+
+    df = pd.DataFrame(submits)
+    return df[:-1]
+
+#search by drawing
+def convert_to_binary(sketch):
+    sketch_gray = cv2.cvtColor(sketch, cv2.COLOR_RGB2GRAY)
+    sketch_binary = np.zeros(sketch_gray.shape[:2])
+    sketch_binary[sketch_gray < 255] = 1
+    
+    return sketch_binary
